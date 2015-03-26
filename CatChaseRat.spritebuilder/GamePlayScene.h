3be60ccaf88b060ba1e3524e72bcb17c04b06376
Anna Wang <anna.why45@gmail.com>
2015-03-26 13:32:30 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ CatChaseRat.spritebuilder/GamePlayScene.h
@@ -0,0 +1,13 @@
+//
+//  GamePlayScene.h
+//  CatChaseRat
+//
+//  Created by Huiyuan Wang on 3/26/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#ifndef CatChaseRat_GamePlayScene_h
+#define CatChaseRat_GamePlayScene_h
+
+
+#endif
