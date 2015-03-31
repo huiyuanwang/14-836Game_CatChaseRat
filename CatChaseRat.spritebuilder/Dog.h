7620ca91ba9750c7268665ccf48d37c6170276b0
Anna Wang <anna.why45@gmail.com>
2015-03-31 19:13:46 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ CatChaseRat.spritebuilder/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  CatChaseRat
+//
+//  Created by Huiyuan Wang on 3/31/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#ifndef CatChaseRat_Dog_h
+#define CatChaseRat_Dog_h
+
+
+#endif
---
+++ CatChaseRat.spritebuilder/Dog.m
@@ -0,0 +1,9 @@
+//
+//  Dog.m
+//  CatChaseRat
+//
+//  Created by Huiyuan Wang on 3/31/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
